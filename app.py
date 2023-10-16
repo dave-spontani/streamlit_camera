acd9cab510ef072e7bf0f269b3bd39274db88292
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-16 11:22:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.title("Try this!")
 st.write("And this!")
-st.write("And maybe even THIS!")
-st.write("Conflict")
-st.write("Also yeah, maybe more?")
+st.write("Conflict here")
+
