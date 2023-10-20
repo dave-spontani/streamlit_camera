eda43d8b0af2fb238628eced30c0d27055f710e7
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-20 08:58:04 +0200

    Revert changes

--- app.py
+++ app.py
@@ -2,14 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plot
 
-from importing import return_even
-
 st.title("Try this!")
 st.write("And this!")
 usage_list = [i for i in range(20)]
 
-all_even = return_even(usage_list)
-
-st.write(all_even)
-
-st.write("Changes are almost instantaneously!")
