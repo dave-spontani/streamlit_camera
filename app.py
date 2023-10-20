e0dc031307a2d7c11eb31b2090773f7bb38751ab
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-20 12:37:20 +0200

    m

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from importing import *
+from backup import *
 
 st.title("Try this!")
 
+first_list = [i for i in range(10)]
+
+all_odd = return_odd(first_list)
+
+all_even = return_even(first_list)
+
+st.write(all_odd)
+
+st.write(all_even)
--- backup.py
+++ backup.py
@@ -1,1 +1,2 @@
-print("Hello World")
+def return_odd(x):
+  return [i for i in x if i % 2 == 1]
