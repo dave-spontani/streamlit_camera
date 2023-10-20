75922cedb839d5139188e6e77b1bd5b947c3e02c
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-20 11:46:29 +0200

    m

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plot
 
 st.title("Try this!")
-st.write("And this!")
-usage_list = [i for i in range(20)]
-
-st.write("And this as well")
-st.write("Trial")
