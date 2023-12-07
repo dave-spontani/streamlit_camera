c1761859d3583df3b52da21966026586a52c0b58
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-12-07 22:52:17 +0100

    Query somehow not working

--- app.py
+++ app.py
@@ -45,6 +45,6 @@ if st.button('Start search!'):
 
     res_df, react_dict = get_data(query)
 
-    st.write(react_dict)
+    # st.write(react_dict)
 
-    st.write(res_df.head())
+    # st.write(res_df.head())
